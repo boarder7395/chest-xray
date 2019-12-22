5c04979304105459abcc921c435d75bd3e2abd92
Tyler Kalbach <tylerkalbach@Tylers-MacBook-Air.local>
2019-12-22 14:11:38 -0500

    add dataset stuff

---
+++ chest_xray/utils/data.py
@@ -0,0 +1,94 @@
+import tensorflow as tf
+import os
+import multiprocessing
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+def parse_fn_random(proto):
+    key_mapping = {
+        'image': tf.io.FixedLenFeature([], tf.string),
+        'label': tf.io.FixedLenFeature([], tf.int64)
+    }
+    features = tf.parse_single_example(proto, key_mapping)
+    
+    # parse image
+    image = features['image']
+    image = tf.io.decode_raw(image, tf.float32)
+    image = tf.reshape(image, (1, 256, 256, 3))
+    image = image / 256
+    
+    # rotate images randomly
+    random_angles = tf.random.uniform(shape = (tf.shape(image)[0], ), 
+                                  minval = -0.3,
+                                  maxval = 0.3)
+
+    rotated_images = tf.contrib.image.transform(
+        image,
+        tf.contrib.image.angles_to_projective_transforms(
+            random_angles, 
+            tf.cast(tf.shape(image)[1], tf.float32), 
+            tf.cast(tf.shape(image)[2], tf.float32),
+        )
+    )
+    return tf.reshape(rotated_images, (256, 256, 3)), features['label']
+
+def parse_fn(proto):
+    key_mapping = {
+        'image': tf.io.FixedLenFeature([], tf.string),
+        'label': tf.io.FixedLenFeature([], tf.int64)
+    }
+    features = tf.parse_single_example(proto, key_mapping)
+    
+    # parse image
+    image = features['image']
+    image = tf.io.decode_raw(image, tf.float32)
+    image = tf.reshape(image, (256, 256, 3))
+    image = image / 256
+    return image, features['label']
+
+def get_dataset(dataset, batch_size=128, parse_fn=parse_fn):
+    dataset = get_raw_dataset(dataset, parse_fn)
+    dataset = dataset.repeat()
+    dataset = dataset.shuffle(buffer_size=512)
+    dataset = dataset.batch(batch_size, drop_remainder=True)
+    dataset = dataset.prefetch(512)
+    dataset.cache()
+    return dataset
+
+def get_raw_dataset(dataset, parse_fn=parse_fn):
+    dataset = tf.data.Dataset.list_files(
+        os.path.join(os.environ['HOME'], 'kaggle_data/chest_xray/{}/tfrecords/*'.format(dataset)),
+    )
+    dataset = dataset.interleave(
+        tf.data.TFRecordDataset,
+        cycle_length=multiprocessing.cpu_count(),
+        num_parallel_calls=multiprocessing.cpu_count()
+    )
+    dataset = dataset.map(parse_fn, 
+                          num_parallel_calls=multiprocessing.cpu_count()-1)
+    return dataset
+
+def get_random_dataset(dataset, batch_size=128, parse_fn=parse_fn):
+    datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+    
+    dataset = get_raw_dataset(dataset, parse_fn)
+    
+    def map_fn(image, y):
+    # path_str is just a normal string here
+        numpy_image = image.numpy()
+        numpy_image = numpy_image.reshape(256, 256, 3)
+        modified_image = datagen.random_transform(numpy_image)
+        return modified_image, y
+
+    dataset = dataset.map(lambda x, y: tf.py_function(map_fn, (x, y,), Tout=(tf.float32, tf.int64)),
+                           num_parallel_calls=multiprocessing.cpu_count())
+    dataset = dataset.repeat()
+    dataset = dataset.shuffle(buffer_size=512)
+    dataset = dataset.batch(batch_size, drop_remainder=True)
+    dataset = dataset.prefetch(512)
+    return dataset
+    
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import logging
+import subprocess
+
+logging.basicConfig()
+logger = logging.getLogger('Chest Xray Install')
+
+def check_nvidia():
+    """Utility function to check if GPU is available. Uses the nvidia-smi utility to 
+    determine if a GPU is available.
+    
+    Returns:
+        bool: Signifies whether a GPU is available.
+    """
+    try:
+        process = subprocess.Popen(['nvidia-smi', '-L'], stdout=subprocess.PIPE)
+    except:
+        return False
+    output, error = process.communicate()
+    if error:
+        return False
+    logger.info('Installing Tensorflow-GPU...')
+    return True
+
+requirements = [
+    'numpy==1.16.1',
+    'tensorflow-gpu==1.13.1' if check_nvidia() else 'tensorflow==1.13.1',
+    'pandas==0.25.3',
+    'matplotlib==3.1.2',
+    'pyspark==2.4.0',
+    'pillow==6.2.1',
+    'scipy==1.3.3',
+    'opencv-python==4.1.2.30'
+]
+
+setup(
+    name='chest_xray',
+    version='0.0.0',
+    python_requires='==3.6.9',
+    packages=find_packages(include=['chest_xray',
+                                    'chest_xray.*']),
+    install_requires=requirements,
+    )
